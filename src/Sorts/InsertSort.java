6cf167691ae6123fed3b8756cdbe69ee3fa1d3a6
FrostyMemes <lager11197@yandex.ru>
2021-09-03 20:17:54 +0300

    Добавлен алгоримт соритировки вставками

---
+++ src/Sorts/InsertSort.java
@@ -0,0 +1,18 @@
+package Sorts;
+
+public class InsertSort {
+
+
+    public int[] Sort (int[] array){
+        for(int i=1;i<array.length;i++)
+            for(int j=i;j>0 && array[j-1]>array[j];j--)
+                swap(array, j-1, j);
+      return array;
+    }
+
+    private void swap(int[] array, int index1, int index2){
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,14 @@ package com.company;
 
 import Sorts.BinaryTreeSort;
 import Sorts.BubbleSort;
+import Sorts.InsertSort;
 import Sorts.MergeSort;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] array1 = new int[] {6,1,7,5,3,3,6,8,9};
-        BubbleSort Sorter = new BubbleSort();
+        InsertSort Sorter = new InsertSort();
         int[] array2 = Sorter.Sort(array1);
         print(array2);
     }
